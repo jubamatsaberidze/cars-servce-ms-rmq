73a66f8b4c53fe07f4e2b902fce2a71fd8363c4d
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-12-16 19:31:23 +0400

    configured cache-module with redis added new feature to download db data to local, as csv used WriteStream to implement streaming for this feature

--- src/app.service.ts
+++ src/app.service.ts
@@ -44,4 +44,69 @@ export class AppService {
         console.error(err);
       });
   }
+
+  async downloadFile(filename: string) {
+    let is_downloaded = false;
+    let offset = 0;
+
+    const filePath = path.join(`download/${filename}.csv`);
+    const filedata = {
+      fileurl: filePath,
+      status: 'In Progress',
+    };
+
+    const writeStream = fs
+      .createWriteStream(filePath, { flags: 'w' })
+      .on('error', (err) => {
+        console.log(err);
+      })
+      .on('finish', () => {
+        logger.debug('Successfully downloaded file.');
+      });
+
+    writeStream.write('id,name,licence_plate');
+
+    while (!is_downloaded) {
+      const data = await this.rabbitMQService.send('load_cars', offset);
+
+      if (!data.length) {
+        is_downloaded = true;
+        filedata.status = 'DONE';
+        break;
+      }
+
+      const CSVString = this.convertToCsv(data);
+      if (CSVString) {
+        logger.debug('Downloading...');
+        writeStream.write(CSVString);
+        offset += 100;
+      }
+    }
+
+    writeStream.end();
+    return filedata;
+  }
+
+  convertToCsv = function (data) {
+    const csvRows = [''];
+    const headers = Object.keys(data[0]);
+
+    for (const row of data) {
+      const values = headers.map((header) => {
+        const val = row[header];
+        return val;
+      });
+
+      csvRows.push(values.join(','));
+    }
+    return csvRows.join('\n');
+  };
+
+  async download(filename: string, res) {
+    try {
+      return res.download(`download/${filename}`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
